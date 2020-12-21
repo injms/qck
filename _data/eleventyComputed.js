316e1adb6d03f62272b0e53400c1d1c13056a1a6
Ian James <injms@users.noreply.github.com>
2020-12-21 20:48:47 +0000

    🖇 Add alternative for each page
    Each page's translation will be able to link to each of the other translations
    thanks to the alternative key.

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,4 +1,5 @@
 const i18next = require('i18next')
+const { dirname } = require('path')
 
 const site = require('./site')
 
@@ -25,5 +26,61 @@ module.exports = {
     }
 
     return 'ltr'
+  },
+
+  // ---------------------------------------------------------------------------
+  // alternativeKey - string
+  // ---------------------------------------------------------------------------
+  // Each page needs a unique key so that any translations can be linked from
+  // one language to the others. For example, a page in English, Spanish, and
+  // French would have different files but have the same key.
+  // The key can be manually set if rqeuired - this will take precedence over
+  // the automatically generated key.
+  // Assumes a folder-per-page set up - not a file-per-page set up; for example:
+  //  → about/
+  //    ↳ about.md
+  //    ↳ about.es.md
+  //    ↳ about.pt-br.md
+  //  → homepage/
+  //    ↳ homepage.md
+  alternativeKey: function ({ alternativeKey, page }) {
+    if (alternativeKey) {
+      return alternativeKey
+    }
+
+    let key = dirname(page.inputPath)
+
+    if (key.endsWith('/') === false) {
+      key += '/'
+    }
+
+    return key
+  },
+
+  // ---------------------------------------------------------------------------
+  // alternatives - array of objects
+  // ---------------------------------------------------------------------------
+  // A list of the page's translations is needed to create `rel="alternate"`
+  // metadata, link between the translations, and provide translations in the
+  // site's RSS feed.
+  alternatives: function ({ collections, alternativeKey, language }) {
+    const i18n = i18next.createInstance()
+    i18n.init(site.i18n)
+
+    const alternativeLanguages = collections.all
+      .filter(({ data }) => data.alternativeKey === alternativeKey) // only this key
+      .filter(({ data }) => data.language !== language) // not this page's language please
+      .map(({ data }) => {
+        const language = data.language.toLowerCase()
+
+        return {
+          language,
+          languageName: i18n.t('locale', { lng: language }),
+          url: data.page.url,
+          pageTitle: data.title,
+        }
+      })
+
+    return alternativeLanguages
   },
 }
