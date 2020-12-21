34aaf7b3a222780a1600f534b81471f7cbb7b4c0
Ian James <injms@users.noreply.github.com>
2020-12-21 16:51:16 +0000

    💽 Add i18next settings to site data file
    Putting the settings in the site data file allows them to be set in one place
    and used to initialise i18next in different places with consistent results.

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,15 @@
+const getTranslations = require('../_helpers/getTranslations')
+
 // All the default data and settings. Default strings go into the i18n files.
 const site = {
   defaultLanguage: 'en-gb',
+}
+
+site.i18n = {
+  debug: false,
+  fallbackLng: site.defaultLanguage,
+  lowerCaseLng: true,
+  resources: getTranslations('_locales'),
 }
 
 module.exports = site
