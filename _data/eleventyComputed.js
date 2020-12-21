8db5094c0afd13d0f01c4ab894f87e8c5467c83b
Ian James <injms@users.noreply.github.com>
2020-12-21 16:55:44 +0000

    ⬅️ Add page direction based on the page's language

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,3 +1,5 @@
+const i18next = require('i18next')
+
 const site = require('./site')
 
 module.exports = {
@@ -9,5 +11,19 @@ module.exports = {
     language, // language is required for slug interpolation
   }) {
     return permalink || '/{{ language }}/{{ title | slug }}/'
+  },
+
+  // ---------------------------------------------------------------------------
+  // direction - string: either 'rtl' or 'ltr'
+  // ---------------------------------------------------------------------------
+  // Returns the text direction for the page's language.
+  direction: function ({ language }) {
+    if (typeof language === 'string') {
+      const i18n = i18next.createInstance()
+      i18n.init()
+      return i18n.dir(language)
+    }
+
+    return 'ltr'
   },
 }
