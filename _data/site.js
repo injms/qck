1d70ac6948bc282bfafc731f696542b74666d2c7
Ian James <injms@users.noreply.github.com>
2021-02-16 21:26:48 +0000

    🗄 Set default site metadata

--- _data/site.js
+++ _data/site.js
@@ -3,7 +3,30 @@ const getTranslations = require('../_helpers/getTranslations')
 // All the default data and settings. Default strings go into the i18n files.
 const site = {
   defaultLanguage: 'en-gb',
+  production: process.env.CONTEXT === 'production',
   pseudoLocalisation: process.env.PSEUDOL10N === 'true',
+  baseURL: process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL,
+  themeColour: '#f0f0f0',
+  rel: {
+    me: [
+      'https://www.ianjamesphotograpy.com',
+      'https://ianjames.photograpy',
+      'https://inj.ms',
+      'https://instagram.com/inj.ms',
+      'https://github.com/injms',
+      'https://twitter.com/_injms_',
+    ],
+    publisher: [
+      'https://inj.ms',
+    ],
+  },
+  twitter: {
+    creator: '@_injms_',
+    site: '@_injms_',
+  },
+  opengraph: {
+    type: 'website',
+  },
 }
 
 site.i18n = {
