a0517d478b956495d09d3759f71f2b011d9beb25
Ian James <injms@users.noreply.github.com>
2023-05-12 16:30:50 +1000

    📆 Move date filters out of dependency
    I only use these filters here, so it sees a lot of effort to keep them in their
    own package - especially when it comes to updating them.

---
+++ _filters/date/humandate.js
@@ -0,0 +1,25 @@
+/**
+ * @param  {string} date Date or datetime string that can be used by `Date.parse()`
+ * @param  {string} [locale] Valid BCP47 string. Defaults to en-GB.
+ * @return {string} Date written in locale given
+ */
+const humanDate = (date, locale = 'en-GB') => {
+  if (typeof date === 'undefined') {
+    throw new Error('No date parameter.')
+  }
+
+  const parsedDate = new Date(date)
+
+  // If the date object is invalid it will return 'NaN' on getTime().
+  if (Number.isNaN(parsedDate.getTime())) {
+    throw new Error('Date could not be parsed.')
+  }
+
+  return parsedDate.toLocaleDateString(locale, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+}
+
+module.exports = humanDate
